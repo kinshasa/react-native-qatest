5470e3794d2f7ea4267e8ad814fa63615c4eff45
kinshasa <liushaobo2005@163.com>
2017-01-23 16:10:07 +0800

    init QATest Project

---
+++ index.js
@@ -0,0 +1,72 @@
+/**
+ * @Author: liushaobo2005@163.com
+ * @Date: 2017.1.23 下午 4:02
+ * @Todo: App首页.
+ */
+import React, {Component} from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View
+} from 'react-native';
+
+export default class QATest extends Component {
+
+
+    constructor(props) {
+        super(props);
+        // 初始状态
+        this.state = {};
+    }
+
+    componentWillMount() {
+
+    }
+
+    componentDidMount() {
+
+    }
+
+    componentDidUnMount() {
+
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome}>
+                    Welcome to React Native!
+                </Text>
+                <Text style={styles.instructions}>
+                    To get started, test
+                </Text>
+                <Text style={styles.instructions}>
+                    Double tap R on your keyboard to reload,{'\n'}
+                    Shake or press menu button for dev menu
+                </Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+});
+
+AppRegistry.registerComponent('QATest', () => QATest);
