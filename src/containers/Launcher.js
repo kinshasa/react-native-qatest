892e4df2c6a231747961ce31ae38c4ab3a929c98
kinshasa <liushaobo2005@163.com>
2017-02-09 16:47:07 +0800

    fix bug @liushaobo2005@163.com

--- src/containers/Launcher.js
+++ src/containers/Launcher.js
@@ -1,8 +1,9 @@
 /**
  * @Author: liushaobo2005@163.com
- * @Date: 2017.1.23 下午 6:58
+ * @Date: 2017.2.9 下午 4:33
  * @Desc: 公共组件 - 渲染的入口
- * @NAME: MainScreen
+ * @NAME: Launcher.js
+ * @LIFECYCLE：http://www.tuicool.com/articles/nu6zInB
  */
 
 import React, {Component, PropTypes} from 'react';
@@ -12,6 +13,7 @@ import {
     Text,
     Image
 } from 'react-native';
+
 import ScrollableTabView, {ScrollableTabBar,} from 'react-native-scrollable-tab-view';
 import TabNavigator from 'react-native-tab-navigator';
 
@@ -20,53 +22,137 @@ import UserCenter from "../containers/user/UserCenter"
 
 const LANCHER_IMG_HOME = require("../assets/home.png");
 const LANCHER_IMG_USERCENTER = require("../assets/home.png");
+
 export default class Launcher extends Component {
 
-    static propTypes = {};
+    /**
+     * 父组件传入的属性值
+     * @type {{style: *, account: *, name: *, isTrue: *, callback: *}}
+     */
+    static propTypes = {
+        style: View.propTypes.style,
+        account: PropTypes.number,
+        name: PropTypes.string,
+        isTrue: PropTypes.bool,
+        callback: PropTypes.func,
+    };
 
-    static defaultProps = {};
+    /**
+     * 父组件传入的数据
+     * @type {{data: {}}}
+     */
+    static defaultProps = {
+        data: {}
+    };
 
+    /**
+     * 构造函数
+     * @param props
+     * @param context
+     */
     constructor(props, context) {
+        console.log("Launcher", "constructor()");
         super(props, context);
         this.state = {
             selectedTab: "T1"
         };
     }
 
+    /**
+     * 当前组件渲染次数
+     * @type {number}
+     */
+    renderCount = 0;
+
+    /**
+     * 组件加载前
+     * 生命周期中仅被调用1次，可以使用SetState
+     */
     componentWillMount() {
-
+        console.log("Launcher", "componentWillMount()");
     }
 
+    /**
+     * 组件加载后
+     * 生命周期中仅被调用1次，可以使用SetState
+     * 用于网络请求和页面渲染
+     */
     componentDidMount() {
-
+        console.log("Launcher", "componentDidMount()");
     }
 
+    /**
+     * 父组件属性值变更监听
+     * 可以使用SetState
+     * @param newProps
+     */
     componentWillReceiveProps(newProps) {
-
+        console.log("Launcher", "componentWillReceiveProps():" + newProps);
     }
 
+    /**
+     * 属性值/状态值变更后，是否需要刷新页面
+     * @param nextProps 是即将被设置的属性，旧的属性还是可以通过 this.props 来获取。
+     * @param nextState 表示组件即将更新的状态值。
+     * @returns {boolean} 默认true, 返回值决定是否需要更新组件，如果 true 表示需要更新，继续走后面的更新流程。
+     */
+    shouldComponentUpdate(nextProps, nextState) {
+        let isUpdate = (this.props != nextProps) || (this.state != nextState);
+        console.log("Launcher", "shouldComponentUpdate():" + isUpdate);
+        return isUpdate;
+    }
+
+    /**
+     * 如果组件状态或者属性改变，并且shouldComponentUpdate返回为 true
+     * @param nextProps 更新之后的属性
+     * @param nextState 更新之后的状态
+     */
+    componentWillUpdate(nextProps, nextState) {
+        console.log("Launcher", "componentWillUpdate()");
+    }
+
+    /**
+     * 更新完成界面之后通知
+     * @param prevProps 更新之前的属性
+     * @param prevState 更新之前的状态
+     * @returns {boolean}
+     */
     componentDidUpdate(prevProps, prevState) {
+        console.log("Launcher", "componentDidUpdate()");
     }
 
+    /**
+     * 组件即将卸载前调用
+     * 在这个函数中，可以做一些组件相关的清理工作，例如取消计时器、网络请求等。
+     */
     componentDidUnMount() {
+        console.log("Launcher", "componentDidUnMount()");
 
     }
 
+    /**
+     * 组件更新
+     * @returns {XML}
+     */
     render() {
+        this.renderCount++;
+        console.log("Launcher", "render():" + this.renderCount);
         return (
-            <View style={{flex: 1}}>
-                <TabNavigator
-                    tabBarStyle={LauncherStyles.tab}
-                    hidesTabTouch={true}
-                >
-                    {this.renderTabItem("T1", "首页", LANCHER_IMG_HOME, LANCHER_IMG_HOME, <HomePage />)}
-                    {this.renderTabItem("T2", "用户中心", LANCHER_IMG_HOME, LANCHER_IMG_HOME, <UserCenter />)}
-                </TabNavigator>
+            <View style={LauncherStyles.container}>
+                {this.renderTable()}
             </View>
-            /*{this.renderBody()}*/
         );
     }
 
+    /**
+     * 返回TableItem组件
+     * @param tag 标记
+     * @param title 标题
+     * @param img 默认的图标
+     * @param selectedImg 选中的图标
+     * @param childView TableItem中的View
+     * @returns {XML}
+     */
     renderTabItem(tag, title, img, selectedImg, childView) {
         return (
             <TabNavigator.Item
@@ -81,12 +167,31 @@ export default class Launcher extends Component {
         );
     }
 
+    /**
+     * 返回TableView组件
+     * @returns {XML}
+     */
+    renderTable() {
+        return (
+            <TabNavigator
+                tabBarStyle={LauncherStyles.tab}
+                hidesTabTouch={true}
+            >
+                {this.renderTabItem("T1", "首页", LANCHER_IMG_HOME, LANCHER_IMG_HOME, <HomePage />)}
+                {this.renderTabItem("T2", "用户中心", LANCHER_IMG_USERCENTER, LANCHER_IMG_USERCENTER, <UserCenter />)}
+            </TabNavigator>
+        );
+    }
+
+    /**
+     * 返回旧的组件
+     * @returns {XML}
+     */
     renderBody() {
         return (
             <ScrollableTabView
                 scrollWithoutAnimation={true}
                 style={LauncherStyles.container}
-                /*隐藏下划线，显示小三角图标*/
                 contentProps={{}}
                 initialPage={0}
                 renderTabBar={() =>  <ScrollableTabBar/>}
@@ -97,6 +202,7 @@ export default class Launcher extends Component {
             </ScrollableTabView>
         );
     }
+
 }
 
 const LauncherStyles = StyleSheet.create({
