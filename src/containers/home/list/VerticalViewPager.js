711ea94b65e821e81b54621f3d699826e7aef288
kinshasa <liushaobo2005@163.com>
2017-03-03 19:59:13 +0800

    modify vertical viewpager. @liushaobo2005@163.com

--- src/containers/home/list/VerticalViewPager.js
+++ src/containers/home/list/VerticalViewPager.js
@@ -59,9 +59,25 @@ export default class VerticalViewPager extends Component {
         console.log("VerticalViewPager", "constructor()");
         super(props, context);
         this.state = {
-            fadeUpAnim: new Animated.Value(0),
-            fadeDownAnim: new Animated.Value(0),
         };
+        this.layout = {
+            scrollViewLayout:{},//ScrollView的布局
+            topViewLayout:{},//TopView的布局
+            bottomViewLayout:{}//BottomView的布局
+        };
+        this.scrollOffset={
+            begin:{},//beginDrag和scrollBegin的最新值
+            end:{},//beginDrag和scrollBegin的最新值
+            beginDrag:{},//ScrollView触摸起始坐标
+            endDrag:{},//ScrollView触摸停止坐标
+            scrollBegin:{},//ScrollView滑动起始坐标
+            scrollEnd:{},//ScrollView滑动停止坐标
+            threshold:0,//下拉可以看到bottomView的距离
+            pos:0,//表示触摸前底部栏所处ScrollView的位置，0表示TopView，1表示BottomView
+            type:0,//0表示下拉，1表示上拉
+            state:false,//表示是否在滚动计算中
+            status:0,//0没有滑动，1开始触摸滑动，2结束触摸，3释放手后自动滑动，4释放手后自动滑动停止(没有自动滑动即2，有即4)
+        }
     }
 
     /**
@@ -132,54 +148,120 @@ export default class VerticalViewPager extends Component {
      * 在这个函数中，可以做一些组件相关的清理工作，例如取消计时器、网络请求等。
      */
     componentWillUnmount() {
-        console.log("VerticalViewPager", `componentWillUnmount()this.refs["test"]:${this.refs["test"]}`);
+        console.log("VerticalViewPager", `componentWillUnmount()`);
 
     }
 
-    //安卓下滚动画
-    pullDown() {
-        InteractionManager.runAfterInteractions(async() => {
-            Animated.spring(this.state.fadeDownAnim, {
-                toValue: 1,   // Returns to the start
-                //velocity: 1,  // Velocity makes it move
-                tension: -10, // Slow
-            }).start();
-            Animated.spring(this.state.fadeUpAnim, {
-                toValue: 1,   // Returns to the start
-                //velocity: 1,  // Velocity makes it move
-                tension: -10, // Slow
-            }).start();
-        });
-
-    }
-
-    //安卓滚上动画
-    pushUp() {
-        InteractionManager.runAfterInteractions(async() => {
-            Animated.spring(this.state.fadeDownAnim, {
-                toValue: 0,   // Returns to the start
-                //velocity: 1,  // Velocity makes it move
-                tension: -10, // Slow
-            }).start();
-            Animated.spring(this.state.fadeUpAnim, {
-                toValue: 0,   // Returns to the start
-                //velocity: 1,  // Velocity makes it move
-                tension: -10, // Slow
-            }).start();
-        });
-    }
-
-    scrollAction(e) {
-        this.layoutH = e.nativeEvent.layoutMeasurement.height;
-        this.contentSizeH = e.nativeEvent.contentSize.height;
-        this.contentOffsetH = e.nativeEvent.contentOffset.y;
-        let count = this.contentSizeH - this.contentOffsetH - this.layoutH;
-
-        if (count < 5) {
-            this.pullDown()
+    onScroll(offset){
+        console.log("VerticalViewPager", `onScroll() e:${JSON.stringify(offset)}；status:${this.scrollOffset.status}`);
+        if(this.scrollOffset.status == 3){
+            //释放触摸后自动滑动中
+            if(this.scrollOffset.type){
+                //下拉中
+                //y为topViewLayout的BottomY值
+                let y = this.layout.topViewLayout.height+this.layout.topViewLayout.y;
+                if(offset.y >= y){
+                    console.log("VerticalViewPager", `componentWillUnmount() status:${this.scrollOffset.status}`);
+                    this.scrollOffset.status = 4;
+                    this.onScrollDown();
+                }
+            }
         }
+
     }
 
+    /**
+     * 下拉到bottomView顶端，或恢复到bottomView顶端
+     */
+    onScrollDown(){
+        console.log("VerticalViewPager", `onScrollDown() ${JSON.stringify(this.layout.bottomViewLayout)}`);
+        this.refs['scrollView'] && this.refs['scrollView'].scrollTo({y:this.layout.bottomViewLayout.y,animated:true});
+    }
+
+    /**
+     * 上拉到topView底端，或恢复到topView底端
+     */
+    onScrollTop(){
+        console.log("VerticalViewPager", `onScrollTop()`);
+        this.refs['scrollView'] && this.refs['scrollView'].scrollTo({y:this.scrollOffset.threshold,animated:true});
+    }
+
+    /**
+     * 滚动计算最终位置
+     * @param type
+     */
+    pull(type){
+        console.log("VerticalViewPager", `pull(${type})state: ${this.scrollOffset.state}`);
+        //如果在计算中则不需要重复计算
+        if(this.scrollOffset.state == true){
+            return;
+        }
+
+        this.scrollOffset.state = true;
+
+        if(this.scrollOffset.beginDrag.y <= this.scrollOffset.endDrag.y){
+            //下拉
+            this.scrollOffset.type = 0;
+        }else{
+            //上拉
+            this.scrollOffset.type = 1;
+        }
+        //下拉可以看到bottomView的距离
+        this.scrollOffset.threshold = this.layout.topViewLayout.height - this.layout.scrollViewLayout.height+this.layout.topViewLayout.y;
+
+        //TopView的底部坐标
+        let topViewBottomY = this.layout.topViewLayout.height+this.layout.topViewLayout.y;
+        //BottomView置顶后底部栏坐标
+        //topViewBottomY = topViewBottomY+this.layout.scrollViewLayout.height;
+        //判断当前所处ScrollView的组件位置
+        if(this.scrollOffset.beginDrag.y <= this.scrollOffset.threshold){
+            this.scrollOffset.pos = 0;
+        }else if(this.scrollOffset.beginDrag.y >= (topViewBottomY+10)){
+            this.scrollOffset.pos = 1;
+        }else{
+            this.scrollOffset.pos = 2;
+        }
+
+        //ScrollView下拉到滑动停止后的距离
+        let y = this.scrollOffset.end.y;
+
+        console.log("VerticalViewPager", `pull(${type})pos: ${this.scrollOffset.pos}`);
+        //下拉
+        if(this.scrollOffset.type == 0){
+            if(this.scrollOffset.pos == 1){
+                //如果起始触摸位置在下面，则不需要计算滚动位置。
+                return;
+            }
+            if(y>(this.scrollOffset.threshold+30)){
+                //下拉到bottomView顶端
+                this.onScrollDown();
+            }else if(y > this.scrollOffset.threshold){
+                //不超过下拉临界值+10，不用下拉，恢复到topView的底端
+                this.onScrollTop();
+            }else{
+                console.log("VerticalViewPager", `cannot pull down.`);
+            }
+        }else{
+            if(this.scrollOffset.pos == 0){
+                //如果起始触摸位置在上面，则不需要计算滚动位置。
+                console.log("VerticalViewPager", `pull(${type})pos: ${this.scrollOffset.pos}`);
+                return;
+            }
+            //上拉可以看到TopView的距离为 TopView底部坐标(TopView的高度和Y轴值)
+            let topViewBottomY = this.layout.topViewLayout.height+this.layout.topViewLayout.y;
+            if(y<(topViewBottomY-30)){
+                //上拉到TopView顶端
+                this.onScrollTop();
+            }else if(y > topViewBottomY){
+                //不超过下拉临界值+10，不用下拉，恢复到topView的底端
+                this.onScrollDown();
+            }else{
+                console.log("VerticalViewPager", `cannot pull up.`);
+            }
+        }
+        console.log("VerticalViewPager", `pull(${type})scrollOffset: ${JSON.stringify(this.scrollOffset)}`);
+        console.log("VerticalViewPager", `pull(${type})layout: ${JSON.stringify(this.layout)}`);
+    }
     /**
      * 组件更新
      * @returns {XML}
@@ -188,41 +270,54 @@ export default class VerticalViewPager extends Component {
         this.renderCount++;
         console.log("VerticalViewPager", "render() renderCount:" + this.renderCount);
 
-        let animatedPull = [
-            {
-                opacity: 1,
-                transform: [{
-                    translateY: this.state.fadeUpAnim.interpolate({
-                        inputRange: [0, 1],
-                        outputRange: [0, height]  // 0 : 150, 0.5 : 75, 1 : 0
-                    }),
-                }],
-            }, {}
-        ];
-
-        let animatedPush = [
-            {
-                opacity: 1,
-                transform: [{
-                    translateY: this.state.fadeDownAnim.interpolate({
-                        inputRange: [0, 1],
-                        outputRange: [0, height]  // 0 : 150, 0.5 : 75, 1 : 0
-                    }),
-                }],
-            }, {}
-        ];
-
         return (
-            <ScrollView onScroll={(e) => {}}
+            <ScrollView
+                pagingEnabled
+                alwaysBounceVertical
+                scrollsToTop
+                scrollEventThrottle={1000}
+                ref="scrollView"
+                onScroll={(e)=>{{this.onScroll(e.nativeEvent.contentOffset)}}}
+                onLayout={(e)=>{this.layout.scrollViewLayout = e.nativeEvent.layout}}
+                onScrollBeginDrag={(e) => {
+                    this.scrollOffset.beginDrag = e.nativeEvent.contentOffset;
+                    this.scrollOffset.begin = e.nativeEvent.contentOffset;
+                    this.scrollOffset.state = false;
+                    this.scrollOffset.status = 1;
+                }}
+                onScrollEndDrag={(e) => {
+                    this.scrollOffset.endDrag = e.nativeEvent.contentOffset;
+                    this.scrollOffset.end = e.nativeEvent.contentOffset;
+                    this.scrollOffset.status = 2;
+                    this.pull('onScrollEndDrag');
+                }}
+                onMomentumScrollBegin={(e) => {
+                    this.scrollOffset.scrollBegin = e.nativeEvent.contentOffset;
+                    //this.scrollOffset.begin = e.nativeEvent.contentOffset;
+                    this.scrollOffset.status = 3;
+                }}
+                onMomentumScrollEnd={(e) => {
+                    this.scrollOffset.end = e.nativeEvent.contentOffset;
+                    this.scrollOffset.scrollEnd = e.nativeEvent.contentOffset;
+                    this.scrollOffset.status = 4;
+                    this.pull('onMomentumScrollEnd');
+                }}
                 style={VerticalViewPagerStyles.container}>
-                <Text style={{padding: 10, backgroundColor: "#999", margin: 5}}>下拉</Text>
-                <Animated.View
-                    style={[animatedPull, {width: width, height: height, backgroundColor: "red"}]}/>
-                <Text style={{padding: 10, backgroundColor: "#999", margin: 5}}>上拉</Text>
-                <Animated.View
-                    style={[animatedPush, {width: width, height: height, backgroundColor: "green"}]}/>
-                {/*<Animated.View
-                 style={[{width: width, height: height, backgroundColor: "#aaa"}]}/>*/}
+                <View
+                    onLayout={(e)=>{this.layout.topViewLayout = e.nativeEvent.layout}}
+                    style={{width, height: height+400, backgroundColor: "red",justifyContent:"flex-end"}}>
+                    <Text
+                        onPress={()=>{this.onScrollDown()}}
+                        style={VerticalViewPagerStyles.criticalView}>下拉</Text>
+
+                </View>
+                <View
+                    onLayout={(e)=>{this.layout.bottomViewLayout = e.nativeEvent.layout}}
+                    style={{width, height: height+400, backgroundColor: "green"}}>
+                    <Text
+                        onPress={()=>{this.onScrollTop()}}
+                        style={VerticalViewPagerStyles.criticalView}>上拉</Text>
+                </View>
                 {/*<HomePage tabLabel="HomePage"/>*/}
                 {/*<Settings tabLabel="Settings"/>*/}
                 {/*<QATest tabLabel="QATest"/>*/}
@@ -249,5 +344,8 @@ const VerticalViewPagerStyles = StyleSheet.create({
         width: width,
         height: height,
         backgroundColor: "#999"
+    },
+    criticalView:{
+        padding: 10, backgroundColor: "#999", margin: 5
     }
 });
