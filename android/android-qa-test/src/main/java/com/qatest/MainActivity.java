d801f26edaebddeebe70505d4301a19948bc4182
kinshasa <liushaobo2005@163.com>
2017-03-08 17:44:22 +0800

    modify

--- android/android-qa-test/src/main/java/com/qatest/MainActivity.java
+++ android/android-qa-test/src/main/java/com/qatest/MainActivity.java
@@ -9,6 +9,7 @@ import com.tencent.bugly.Bugly;
 public class MainActivity extends ReactActivity {
 
     private Context context = this;
+
     /**
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
