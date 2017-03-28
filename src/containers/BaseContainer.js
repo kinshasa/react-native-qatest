a031c3903204da29fb67b57b417277a48e60e291
kinshasa <liushaobo2005@163.com>
2017-03-28 17:30:06 +0800

    modify files. @liushaobo2005@163.com

---
+++ src/containers/BaseContainer.js
@@ -0,0 +1,115 @@
+/**
+ * @Author: liushaobo2005@163.com
+ * @Date: 2017.3.28 下午 5:25
+ * @Desc: 公共组件 - BaseContainer
+ * @Name: BaseContainer.js
+ * @LifeCycle：https://github.com/kinshasa/react-native-qatest
+ */
+
+import React, {Component, PropTypes} from 'react';
+import {
+    StyleSheet,
+    View,
+    Text,
+} from 'react-native';
+
+export default class BaseContainer extends Component {
+
+    static propTypes = {
+        style: View.propTypes.style,
+        account: PropTypes.number,
+        name: PropTypes.string,
+        isTrue: PropTypes.bool,
+        callback: PropTypes.func,
+    };
+
+    static defaultProps = {
+        data: {}
+    };
+
+    constructor(props, context) {
+        console.log("BaseContainer constructor()");
+        super(props, context);
+        this.state = {};
+    }
+
+    /**
+     * 当前组件渲染次数
+     * @type {number}
+     */
+    count = 0;
+
+    componentWillMount() {
+        console.log("BaseContainer componentWillMount()", new Date());
+    }
+
+    componentDidMount() {
+        console.log("BaseContainer componentDidMount()", new Date());
+    }
+
+    /**
+     * 父组件属性值变更监听
+     * 可以使用SetState
+     * @param newProps
+     */
+    componentWillReceiveProps(newProps) {
+        console.log("BaseContainer componentWillReceiveProps()", newProps);
+    }
+
+    /**
+     * 属性值/状态值变更后，是否需要刷新页面
+     * @param nextProps 是即将被设置的属性，旧的属性还是可以通过 this.props 来获取。
+     * @param nextState 表示组件即将更新的状态值。
+     * @returns {boolean} 默认true, 返回值决定是否需要更新组件，如果 true 表示需要更新，继续走后面的更新流程。
+     */
+    shouldComponentUpdate(nextProps, nextState) {
+        let isUpdate = (this.props != nextProps) || (this.state != nextState);
+        console.log("BaseContainer shouldComponentUpdate()", isUpdate);
+        return isUpdate;
+    }
+
+    /**
+     * 如果组件状态或者属性改变，并且shouldComponentUpdate返回为 true
+     * @param nextProps 更新之后的属性
+     * @param nextState 更新之后的状态
+     */
+    componentWillUpdate(nextProps, nextState) {
+        console.log("BaseContainer componentWillUpdate()", new Date());
+    }
+
+    /**
+     * 更新完成界面之后通知
+     * @param prevProps 更新之前的属性
+     * @param prevState 更新之前的状态
+     * @returns {boolean}
+     */
+    componentDidUpdate(prevProps, prevState) {
+        console.log("BaseContainer componentDidUpdate()", new Date());
+    }
+
+    /**
+     * 组件即将卸载前调用
+     * 在这个函数中，可以做一些组件相关的清理工作，例如取消计时器、网络请求等。
+     */
+    componentWillUnmount() {
+        console.log("BaseContainer componentWillUnmount()");
+
+    }
+
+    render() {
+        this.count++;
+        console.log("BaseContainer render() count:", this.count);
+        return (
+            <View style={BaseContainerStyles.container}>
+
+            </View>
+        );
+    }
+
+}
+
+const BaseContainerStyles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+});
--- src/scenes.js
+++ src/scenes.js
@@ -21,22 +21,26 @@ import GoodsDetailPageContainer from "./containers/home/list/GoodsDetailPageCont
 import HotFixPushyContainer from "./containers/home/list/HotFixPushyContainer";
 
 import CustomListView from "./containers/test/list/CustomListView";
+import InheritanceBaseContainer from "./containers/test/list/InheritanceBaseContainer";
 
 export default  scenes = Actions.create(
     <Scene key="root">
-        <Scene role="root" key="Launcher" component={Launcher} des={"启动器"} hideNavBar={true}/>
-        <Scene role="home" key="CartAnimationContainer" component={CartAnimationContainer} des={"购物车动画"} hideNavBar={true}/>
-        <Scene role="home" key="AnimationDemoContainer" component={AnimationDemoContainer} des={"动画Demo"} hideNavBar={true}/>
-        <Scene role="home" key="LayoutXYDemoContainer" component={LayoutXYDemoContainer} des={"Layout布局"} hideNavBar={true}/>
+        <Scene role="root" key="Launcher" component={Launcher} des={"启动器"} hideNavBar/>
+        <Scene role="home" key="CartAnimationContainer" component={CartAnimationContainer} des={"购物车动画"} hideNavBar/>
+        <Scene role="home" key="AnimationDemoContainer" component={AnimationDemoContainer} des={"动画Demo"} hideNavBar/>
+        <Scene role="home" key="LayoutXYDemoContainer" component={LayoutXYDemoContainer} des={"Layout布局"} hideNavBar/>
         <Scene role="home" key="IconCollectContainer" component={IconCollectContainer} des={"图标库"} hideNavBar={false}/>
         <Scene role="home" key="SwipeListContainer" component={SwipeListContainer} des={"Swipe组件"}/>
-        <Scene role="home" key="VerticalViewPagerContainer" component={VerticalViewPagerContainer} des={"VerticalViewPagerContainer"}/>
+        <Scene role="home" key="VerticalViewPagerContainer" component={VerticalViewPagerContainer} des={"垂直ViewPager"}/>
         <Scene role="home" key="TabViewContainer" component={TabViewContainer} des={"scrollable-tab-view"}/>
-        <Scene role="home" key="NestScrollViewContainer" component={NestScrollViewContainer} des={"NestScrollViewContainer"} hideNavBar={true}/>
-        <Scene role="home" key="GoodsDetailPageContainer" component={GoodsDetailPageContainer} des={"商品详情页"} hideNavBar={true}/>
-        <Scene role="home" key="DropDownAlertContainer" component={DropDownAlertContainer} des={"DropDownAlertContainer"}
-               hideNavBar={true}/>
-        <Scene role="home" key="HotFixPushyContainer" component={HotFixPushyContainer} des={"react-native-HotFixPushyContainer"} hideNavBar={true}/>
-        <Scene role="QATest" key="CustomListView" component={CustomListView} des={"1万行数据的ListView"} hideNavBar={true}/>
+        <Scene role="home" key="NestScrollViewContainer" component={NestScrollViewContainer} des={"内嵌ViewPager"}/>
+        <Scene role="home" key="GoodsDetailPageContainer" component={GoodsDetailPageContainer} des={"商品详情页"}/>
+        <Scene role="home" key="DropDownAlertContainer" component={DropDownAlertContainer}
+               des={"DropDownAlertContainer"}/>
+        <Scene role="home" key="HotFixPushyContainer" component={HotFixPushyContainer} des={"react-native-pushy"}/>
+
+        <Scene role="QATest" key="CustomListView" component={CustomListView} des={"1万行数据的ListView"}/>
+        <Scene role="QATest" key="InheritanceBaseContainer" component={InheritanceBaseContainer}
+               des={"集成BaseContainer"}/>
     </Scene>
 );
