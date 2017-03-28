9b430cdff1010a7944a7faeffe76cbdfe72223de
kinshasa <liushaobo2005@163.com>
2017-03-28 15:15:16 +0800

    add react-native-dropdownalert. @liushaobo2005@163.com

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@
  * @LIFECYCLE：http://www.tuicool.com/articles/nu6zInB
  */
 import React, {Component} from "react";
-import {View, AppRegistry, Linking} from "react-native";
+import {AppRegistry, InteractionManager, Linking, StyleSheet, View} from "react-native";
 
 import MainProvider from "./MainProvider";
 import Http from "../common/utils/Http";
 import config from "../common/config";
-import DropdownAlert from 'react-native-dropdownalert'
+import DropdownAlert from "react-native-dropdownalert";
 
 
 export default class App extends Component {
@@ -30,31 +30,72 @@ export default class App extends Component {
 
     componentDidMount() {
         console.log('App componentDidMount() config:', config);
-
-        Linking.getInitialURL().then((url) => {
-            if (url) {
-                console.log('Initial url is: ' + url);
-            }
-        }).catch(err => console.error('An error occurred', err));
-
-        Linking.addEventListener('url', (event) => {
-            console.log('event:', event);
+        InteractionManager.runAfterInteractions(() => {
+            // ...耗时较长的同步的任务...
+            this.addEventListener();
+            this.showDropDownAlert('info','App周期信息','App初始化完成');
         });
+
 
     }
 
+    addEventListener = () => {
+        Linking.addEventListener('url', (event) => {
+            this.showDropDownAlert('info','收到外部调用',JSON.stringify(event));
+        });
+    };
+
+
     componentWillUnmount() {
         console.log('App componentWillUnmount()');
+        this.rmEventListener();
+    }
+
+    rmEventListener = () => {
         Linking.removeEventListener('url', (event) => {
-            console.log('event2:', event);
+            console.log('App rmEventListener() event:', event);
         });
+    };
+
+
+    showDropDownAlert = (type,title,msg) => {
+        console.log('App showDropDownAlert() event:', msg);
+        setTimeout(() => {
+            this.dropdown && this.dropdown.alertWithType(type, title, msg);
+        }, 500);
+
+    };
+
+    closeDropDownAlert(data) {
+        console.log('App closeDropDownAlert() data:', data);
+        setTimeout(() => {
+            this.dropdown && this.dropdown.onClose();
+        }, 2000);
     }
 
     render() {
         return (
-            <MainProvider />
+            <View style={AppStyles.container}>
+                <MainProvider />
+                <DropdownAlert
+                    ref={(ref) => this.dropdown = ref}
+                    titleNumOfLines={0}
+                    messageNumOfLines={0}
+                    closeInterval={4000}
+                    showCancel={false}
+                    imageSrc={'https://facebook.github.io/react/img/logo_og.png'}
+                    containerStyle={{backgroundColor: '#6441A4'}}
+                    onCancel={(data) => {
+                    }}
+                    onClose={(data) => {
+                    }}/>
+            </View>
         );
     }
 }
-
+const AppStyles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+});
 AppRegistry.registerComponent('QATest', () => App);
