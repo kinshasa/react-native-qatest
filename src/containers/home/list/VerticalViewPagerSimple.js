bd252e45c2e237921110300d95ec58897c5ced8f
kinshasa <liushaobo2005@163.com>
2017-03-28 15:35:35 +0800

    add react-native-dropdownalert. @liushaobo2005@163.com

--- src/containers/home/list/VerticalViewPagerSimple.js
+++ src/containers/home/list/VerticalViewPagerSimple.js
@@ -6,151 +6,18 @@
  * @LifeCycle：http://www.tuicool.com/articles/nu6zInB
  */
 
-import React, {Component, PropTypes} from 'react';
-import {
-    StyleSheet,
-    View,
-    Text,
-    Dimensions,
-    ScrollView
-} from 'react-native';
+import React, {Component} from "react";
+import {Dimensions, StyleSheet, View} from "react-native";
 
-import VerticalViewPager from '../../../components/viewpager/VerticalViewPager'
-import Settings from '../../set/Setting'
-import TabView from './TabView'
+import GoodsDetailReact from "../../../components/viewpager/GoodsDetailReact";
 
 const {height, width} = Dimensions.get('window');
 export default class VerticalViewPagerSimple extends Component {
 
-    constructor(props, context){
-        super(props, context);
-        this.state = {
-            refresh: false
-        };
-
-    }
-
-    //scrollableTabView的子组件数组
-    scrollTabViewChildren = {};
-    //verticalVP的Layout数据
-    verticalLayout = {};
-
-    //tabBar的Layout数据
-    tabBarLayout={};
-
-    //tabBar是否显示
-    tabBarDisplay=false;
-
-    componentDidMount() {
-    }
-
-    /**
-     * 滚动到底部栏成功后回调
-     */
-    onScrollDownComplete(){
-        //如果tabBar没有显示显示，则显示
-        if(this.tabBar && !this.tabBarDisplay){
-            this.tabBarDisplay =true;
-            console.log("VerticalViewPagerSimple onScrollDownComplete",this.tabBar.props.style);
-            this.tabBar.setNativeProps({style:{opacity:1/*,maxHeight:200*/}})
-        }
-
-    }
-
-    /**
-     * 滚动到顶部栏成功后回调
-     */
-    onScrollTopComplete(){
-        //如果tabBar有显示显示，则隐藏
-        if(this.tabBar && this.tabBarDisplay){
-            this.tabBarDisplay =false;
-            console.log("VerticalViewPagerSimple onScrollTopComplete",this.tabBar.props.style);
-            this.tabBar.setNativeProps({style:{opacity:0/*,maxHeight:0*/}})
-        }
-    }
-
-    /**
-     * 懒加载成功后回调
-     */
-    onLazyViewLoadComplete(){
-
-        console.log("VerticalViewPagerSimple onLazyViewLoadComplete()");
-        this.setState({
-            refresh:true
-        })
-
-    }
-
-    onScroll(offset){
-        let tabBarY = this.tabBarLayout.height||50;
-        let verticalY = this.verticalLayout.y||0;
-        let verticalH = this.verticalLayout.height||0;
-        if(offset.y <= (tabBarY+verticalH+verticalY)){
-            this.onScrollTopComplete();
-        } else{
-            this.onScrollDownComplete();
-        }
-
-    }
-
-    onTabBarLayout(layout){
-        if(layout.height>10){
-            this.tabBarLayout =layout;
-        }
-        console.log("VerticalViewPagerSimple onTabBarLayout():",layout);
-    }
-
-    renderTabBar() {
-        console.log("VerticalViewPagerSimple renderTabBar()");
-        if(this.state.refresh){
-            console.log('VerticalViewPagerSimple renderTabBar() verticalView',this.refs['verticalView'].props.children);
-            console.log('VerticalViewPagerSimple renderTabBar() verticalView',this.refs['tabView']);
-
-            if (!this.refs['tabView'].refs['scrollView'].refs['scrollableTabView']) {
-                return;
-            }
-            //需要使用自定义scrollableTabView，且设置了ref为scrollableTabView前提下。
-            let ref = this.refs['tabView'].refs['scrollView'].refs['scrollableTabView'].props.children;
-            //ref = this.refs['verticalView'].refs['scrollView'].refs['scrollableTabView'].props.children;
-            /*if (React.isValidElement(ref[0])) {
-             this.scrollTabViewChildren[0] = ref[0];
-             }*/
-            this.scrollTabViewChildren = ref.map((child, index)=>{
-                if (React.isValidElement(child)) {
-                    console.log("VerticalViewPagerSimple child right! :", index);
-                    return child;
-                }else{
-                    console.log("VerticalViewPagerSimple not child! :", index);
-                }
-            });
-        }
-
-        return (
-            <View
-                onLayout={(e)=>{this.onTabBarLayout(e.nativeEvent.layout)}}
-                ref={(ref)=>{this.tabBar = ref}}
-                style={{width,backgroundColor:"#999",position:'absolute',zIndex:2}}>
-                {/*取ScrollableTabView的顶部栏布局引用 scrolltabViewChildren[0]*/}
-                {this.state.refresh && this.scrollTabViewChildren[0]}
-            </View>
-        )
-    }
-
     render() {
         return (
             <View style={VerticalViewPagerSimpleStyles.container}>
-
-                <VerticalViewPager
-                    ref="verticalView"
-                    onScroll={(offset)=>{this.onScroll(offset)}}
-                    onScrollDownComplete={()=>{this.onScrollDownComplete()}}
-                    onScrollTopComplete={()=>{this.onScrollTopComplete()}}
-                    onLazyViewLoadComplete={()=>{this.onLazyViewLoadComplete()}}>
-                    <Settings onLayout={(e)=>{this.verticalLayout =e.nativeEvent.layout}}/>
-                    <TabView ref='tabView'/>
-                </VerticalViewPager>
-                {this.renderTabBar()}
-
+                <GoodsDetailReact/>
             </View>
         );
     }
@@ -158,6 +25,6 @@ export default class VerticalViewPagerSimple extends Component {
 
 const VerticalViewPagerSimpleStyles = StyleSheet.create({
     container: {
-        flex:1,
+        flex: 1,
     },
 });
