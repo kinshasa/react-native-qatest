83d9d14146f0ffc8a8e5a97d0e64d9785b90e153
kinshasa <liushaobo2005@163.com>
2017-07-12 18:04:22 +0800

    尝试在Fragment和Activity中多次使用不同module的ReactNative组件，后期可以仿照RCT使用代理所有场景。

--- android/android-qa-test/src/main/java/com/android/qatest/ui/ReactRootViewActivity.java
+++ android/android-qa-test/src/main/java/com/android/qatest/ui/ReactRootViewActivity.java
@@ -1,15 +1,16 @@
 package com.android.qatest.ui;
 
-import com.android.qatest.BuildConfig;
-import com.android.qatest.rct.RCTImagePackage;
-import com.android.qatest.rct.RCTViewPackage;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
 import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.ReactRootView;
 import com.facebook.react.common.LifecycleState;
 import com.facebook.react.shell.MainReactPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.react.rnspinkit.RNSpinkitPackage;
 
-import net.xicp.liushaobo.react.ReactRootActivity;
+import net.xicp.liushaobo.react.RCTRootActivity;
 
 import cn.reactnative.modules.update.UpdatePackage;
 
@@ -17,22 +18,33 @@ import cn.reactnative.modules.update.UpdatePackage;
  * Created by lshaobocsu@gmail.com on 2017.5.25.
  */
 
-public class ReactRootViewActivity extends ReactRootActivity {
+public class ReactRootViewActivity extends RCTRootActivity {
+
     @Override
-    protected ReactInstanceManager getReactInstanceManager() {
-        ReactInstanceManager manager = ReactInstanceManager.builder()
-                .setApplication(getApplication())
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mReactRootView = new ReactRootView(this);
+        mReactInstanceManager = ReactInstanceManager.builder()
+                .setApplication(getApplication())//如果在lib module中可能获取不到。
                 .setBundleAssetName("index.android.bundle")
                 .setJSMainModuleName("index.android")
                 .addPackage(new MainReactPackage())
                 .addPackage(new VectorIconsPackage())
                 .addPackage(new UpdatePackage())
-                .addPackage(new RCTImagePackage())
-                .addPackage(new RCTViewPackage())
+                .addPackage(new net.xicp.liushaobo.react.RCTImagePackage())
+                .addPackage(new net.xicp.liushaobo.react.RCTViewPackage())
                 .addPackage(new RNSpinkitPackage())
-                .setUseDeveloperSupport(BuildConfig.DEBUG)
+                .setUseDeveloperSupport(net.xicp.liushaobo.react.BuildConfig.DEBUG)
                 .setInitialLifecycleState(LifecycleState.RESUMED)
                 .build();
-        return manager;
+
+        // 注意这里的HelloWorld必须对应“index.android.js”中的
+        // “AppRegistry.registerComponent()”的第一个参数
+        Bundle bundle = new Bundle();
+        bundle.putString("key1", "value1");
+        mReactRootView.startReactApplication(mReactInstanceManager, "QATest", bundle);
+
+        setContentView(mReactRootView);
     }
 }
