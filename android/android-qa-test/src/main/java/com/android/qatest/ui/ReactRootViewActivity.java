15410588899dd543c1b1ef4792d339ea3c8de181
kinshasa <liushaobo2005@163.com>
2017-07-14 10:01:44 +0800

    移除主项目中的react native模块，新建RN模块独立开发

--- android/android-qa-test/src/main/java/com/android/qatest/ui/ReactRootViewActivity.java
+++ android/android-qa-test/src/main/java/com/android/qatest/ui/ReactRootViewActivity.java
@@ -7,12 +7,9 @@ import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactRootView;
 import com.facebook.react.common.LifecycleState;
 import com.facebook.react.shell.MainReactPackage;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.react.rnspinkit.RNSpinkitPackage;
 
+import net.xicp.liushaobo.react.BuildConfig;
 import net.xicp.liushaobo.react.RCTRootActivity;
-
-import cn.reactnative.modules.update.UpdatePackage;
 
 /**
  * Created by lshaobocsu@gmail.com on 2017.5.25.
@@ -23,28 +20,21 @@ public class ReactRootViewActivity extends RCTRootActivity {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (mReactRootView == null) {
+            mReactRootView = new ReactRootView(this);
+            mReactInstanceManager = ReactInstanceManager.builder()
+                    .setApplication(getApplication())
+                    .setBundleAssetName("index.android.bundle")
+                    .setJSMainModuleName("index.android")
+                    .addPackage(new MainReactPackage())
+                    .setUseDeveloperSupport(BuildConfig.DEBUG)
+                    .setInitialLifecycleState(LifecycleState.RESUMED)
+                    .build();
 
-        mReactRootView = new ReactRootView(this);
-        mReactInstanceManager = ReactInstanceManager.builder()
-                .setApplication(getApplication())//如果在lib module中可能获取不到。
-                .setBundleAssetName("index.android.bundle")
-                .setJSMainModuleName("index.android")
-                .addPackage(new MainReactPackage())
-                .addPackage(new VectorIconsPackage())
-                .addPackage(new UpdatePackage())
-                .addPackage(new net.xicp.liushaobo.react.RCTImagePackage())
-                .addPackage(new net.xicp.liushaobo.react.RCTViewPackage())
-                .addPackage(new RNSpinkitPackage())
-                .setUseDeveloperSupport(net.xicp.liushaobo.react.BuildConfig.DEBUG)
-                .setInitialLifecycleState(LifecycleState.RESUMED)
-                .build();
-
-        // 注意这里的HelloWorld必须对应“index.android.js”中的
-        // “AppRegistry.registerComponent()”的第一个参数
-        Bundle bundle = new Bundle();
-        bundle.putString("key1", "value1");
-        mReactRootView.startReactApplication(mReactInstanceManager, "QATest", bundle);
-
-        setContentView(mReactRootView);
+            // 注意这里的HelloWorld必须对应“index.android.js”中的
+            // “AppRegistry.registerComponent()”的第一个参数
+            mReactRootView.startReactApplication(mReactInstanceManager, "QATest", null);
+        }
     }
+
 }
--- android/react-native-module/src/main/java/net/xicp/liushaobo/react/RCTRootActivity.java
+++ android/react-native-module/src/main/java/net/xicp/liushaobo/react/RCTRootActivity.java
@@ -9,25 +9,26 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
 
+import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.ReactRootView;
 import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler;
 
 /**
  * Created by lshaobocsu@gmail.com on 2017.5.25.
- *     java.lang.RuntimeException: com.facebook.react.devsupport.JSException: Could not get BatchedBridge, make sure your bundle is packaged correctly
- *     at com.facebook.react.bridge.DefaultNativeModuleCallExceptionHandler.handleException(DefaultNativeModuleCallExceptionHandler.java:24)
+ * java.lang.RuntimeException: com.facebook.react.devsupport.JSException: Could not get BatchedBridge, make sure your bundle is packaged correctly
+ * at com.facebook.react.bridge.DefaultNativeModuleCallExceptionHandler.handleException(DefaultNativeModuleCallExceptionHandler.java:24)
  */
 
-public abstract class RCTRootActivity extends AppCompatActivity{
+public abstract class RCTRootActivity extends AppCompatActivity implements DefaultHardwareBackBtnHandler {
 
     protected static final int OVERLAY_PERMISSION_REQ_CODE = 1;
-    protected RCTRootManager mRCTRootManager;
+    public ReactInstanceManager mReactInstanceManager;
+    public ReactRootView mReactRootView;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        mRCTRootManager = new RCTRootManager(this,"QATest");
-        setContentView(mRCTRootManager.getReactRootView());
 
         //打开悬浮窗口权限
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
@@ -39,35 +40,53 @@ public abstract class RCTRootActivity extends AppCompatActivity{
         }
     }
 
+    public ReactRootView getReactRootView() {
+
+        return mReactRootView;
+    }
+
+    public ReactInstanceManager getReactInstanceManager() {
+        return mReactInstanceManager;
+    }
+
     @Override
     protected void onPause() {
         super.onPause();
-        mRCTRootManager.onPause();
+        if (getReactInstanceManager() != null) {
+            getReactInstanceManager().onHostPause(this);
+        }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        mRCTRootManager.onResume();
+        if (getReactInstanceManager() != null) {
+            getReactInstanceManager().onHostResume(this, this);
+        }
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        mRCTRootManager.onDestroy();
+        if (getReactInstanceManager() != null) {
+            getReactInstanceManager().onHostDestroy(this);
+        }
     }
 
     @Override
     public void onBackPressed() {
-        mRCTRootManager.onBackPressed();
+        if (getReactInstanceManager() != null) {
+            getReactInstanceManager().onBackPressed();
+        }
+
     }
 
     @Override
     public boolean onKeyUp(int keyCode, KeyEvent event) {
-        if (keyCode == KeyEvent.KEYCODE_MENU) {
-            return mRCTRootManager.onKeyUp(keyCode,event);
+        if (keyCode == KeyEvent.KEYCODE_MENU && getReactInstanceManager() != null) {
+            getReactInstanceManager().onBackPressed();
         }
-        return super.onKeyUp(keyCode,event);
+        return super.onKeyUp(keyCode, event);
 
     }
 
@@ -80,5 +99,11 @@ public abstract class RCTRootActivity extends AppCompatActivity{
                 }
             }
         }
+    }
+
+
+    @Override
+    public void invokeDefaultOnBackPressed() {
+        this.onBackPressed();
     }
 }
